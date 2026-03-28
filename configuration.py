5f88380cfa3d46cc2d555e2f1f4b46611af4fb50
David Antonio Villalobos Lozano <tu_correo@example.com>
2026-03-28 11:10:53 -0600

    update: nueva URL servidor

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://cnt-38bf1d4a-a6f1-4c51-87ac-bc071a6f3418.containerhub.tripleten-services.com"
+BASE_URL = "https://cnt-6f6009ce-05f3-4f69-8844-4d9bdbd57003.containerhub.tripleten-services.com"
 
 CREATE_USER_PATH = "/api/v1/users"
 CREATE_KIT_PATH = "/api/v1/kits"
